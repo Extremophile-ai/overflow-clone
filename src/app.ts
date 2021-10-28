bdb892b80ab904fffdc37bffa4a65d919e4a946c
Godspower Uche <godspowercuche56@gmail.com>
2021-10-28 16:45:13 +0100

    change port

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
