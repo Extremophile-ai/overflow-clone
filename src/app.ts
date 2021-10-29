b73f80be5a83e9f6f9de21a29b69da47facff24e
Godspower Uche <godspowercuche56@gmail.com>
2021-10-29 23:04:03 +0100

    welcome message udate

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ connection();
 app.use('/app', routes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to stack overflow');
+  res.send('Welcome to the new stack overflow');
 });
 
 app.listen(port, () => {
