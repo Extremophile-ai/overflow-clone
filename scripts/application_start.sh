291a1aca64dba0ba349e9ed4e0d83b6a32934a9d
Godspower Uche <godspowercuche56@gmail.com>
2021-10-31 08:14:00 +0100

    update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,5 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-node dist/app.js > dist/app.out.log 2> dist/app.err.log < /dev/null & 
+npm run dev
+#node dist/app.js > dist/app.out.log 2> dist/app.err.log < /dev/null & 
